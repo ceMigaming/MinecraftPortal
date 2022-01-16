19dccd56dd870d042400f3d546228218866fa06d
ceMigaming <maciejchamera2003@interia.pl>
2022-01-16 21:35:35 +0100

    Turret now detects players

--- src/main/java/com/cemi/portalreloaded/entity/EntityTurret.java
+++ src/main/java/com/cemi/portalreloaded/entity/EntityTurret.java
@@ -1,10 +1,13 @@
 package com.cemi.portalreloaded.entity;
 
 import java.util.Collections;
+import java.util.List;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumHandSide;
 import net.minecraft.world.World;
 
@@ -13,6 +16,7 @@ public class EntityTurret extends EntityLivingBase {
 	public boolean shouldOpen = false;
 	public boolean isOpen = false;
 	public float openingSpeed = 0.1f;
+	int shootingTimer = 0;
 	
 	public EntityTurret(World worldIn) {
 		super(worldIn);
@@ -20,7 +24,26 @@ public class EntityTurret extends EntityLivingBase {
 
 	@Override
 	public void onUpdate() {
-		
+		List<EntityPlayer> players = world.getEntitiesWithinAABB(EntityPlayer.class, getEntityBoundingBox().grow(15.d));
+		for (EntityPlayer player : players) {
+			if(player.getPositionVector().subtract(getPositionVector()).normalize().dotProduct(getForward()) <= .5d) {
+				shouldOpen = false;
+			}
+			if(player.getPositionVector().subtract(getPositionVector()).normalize().dotProduct(getForward()) > .5d) {
+				shouldOpen = true;
+				if(isOpen) {
+					shootingTimer = (shootingTimer + 1) % 10;
+					if(shootingTimer == 0) {
+						System.out.println("TEST");
+						System.out.println(player.attackEntityFrom(DamageSource.GENERIC, 3.f));
+						//player.knockBack(this, 2.f, 0.d, 0.d);
+					}
+				}
+			}
+		}
+		if(players.size() == 0) {
+			shouldOpen = false;
+		}
 		super.onUpdate();
 	}
 	
